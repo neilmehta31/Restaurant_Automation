4bbcd0709bc9ad206f05e3ea1f1ce536556cd939
SahajGupta <sahajgupta011@gmail.com>
2021-02-26 00:50:09 +0530

    Basic Frontend Made

---
+++ Frontend/node_course/app.js
@@ -0,0 +1,5 @@
+const logger=require('./logger');
+
+logger=1;
+
+logger.log('message');
---
+++ Frontend/node_course/logger.js
@@ -0,0 +1,9 @@
+
+
+
+function log(message){
+    //Send an HTTP request
+    console.log(message);
+}
+
+module.exports.log=log;
