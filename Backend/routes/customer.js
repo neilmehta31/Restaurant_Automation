c87bae215a7a9f8895a0a96c92a70b22d8fa7a28
Neil Mehta <neil.mehta310501@gmail.com>
2021-03-14 16:31:00 +0530

    A dummy pull request for code quality check

--- Backend/routes/customer.js
+++ Backend/routes/customer.js
@@ -13,6 +13,7 @@ router.route('/delete/:id').delete((req, res) => {
         .catch(err => res.status(400).json('Error : ' + err));
 })
 
+
 router.route('/add').post((req, res) => {
     const firstname = req.body.firstname;
     const surname = req.body.surname;
