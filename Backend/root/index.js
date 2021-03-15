76125e1040663adefeedfe07109ba40bc60ded61
Barun <agarwalbarun7@gmail.com>
2021-03-15 13:10:52 +0530

    google auth implemented

--- Backend/root/index.js
+++ Backend/root/index.js
@@ -18,7 +18,7 @@ app.get('/', function(req, res) {
   res.render('pages/auth');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port , () => console.log('App listening on port ' + port));
 
 // index.js
@@ -54,7 +54,7 @@ const GOOGLE_CLIENT_SECRET = 'FhjtGyB0RNo2kH7p7w7C6lz2';
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: "http://localhost:5000/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
       userProfile=profile;
