82e09a7a81242f3f87c2c3e00747352c6df84fc4
Barun-7 <f20190157@hyderabad.bits-pilani.ac.in>
2021-04-23 20:14:02 +0530

    sign in added

--- Frontend/src/views/login/index.jsx
+++ Frontend/src/views/login/index.jsx
@@ -1,44 +1,62 @@
 import "./styles.css";
 import Admin from "../../layouts/Admin.js";
 import RTL from "../../layouts/Manager.js";
-import { withRouter } from "react-router-dom";
+import {useHistory} from 'react-router-dom';
 
 import React, { Component } from "react";
 import Manager from "../../layouts/Manager.js";
+import {useState} from 'react';
+import Axios from 'axios';
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
-  }
+function Login (){
+  
 
-  state = {
-    container: "container",
+  const [container,setContainer]=useState('container');
+  const[email,setEmail]=useState('');
+  const[password,setPassword]=useState('');
+
+  const history=useHistory();
+
+  const signUpButton = () => {
+    setContainer({ container: "container right-panel-active" });
   };
 
-  signUpButton = () => {
-    this.setState({ container: "container right-panel-active" });
+  const signInButton = () => {
+    setContainer({ container: "container" });
   };
 
-  signInButton = () => {
-    this.setState({ container: "container" });
-  };
+  const redirectToCustomer = () => {
+    
+        Axios.post("http://localhost:5000/api/customer/signin", {
+        email,
+        password
+      }).then((response) => {
+        if(response.data.success)
+        {
+          history.push("/admin/dashboard");
+          console.log(email);
+          
+        }
+        console.log(response.data.success);
+      }).catch((err) => {
+        console.log(err);
+      });
 
-  redirectToCustomer = () => {
-    console.log("meow");
-    this.props.history.push("/admin/dashboard");
-  };
-  redirectToManager = () => {
-    this.props.history.push("/manager/setMenu");
-  };
+      
+      };
+      
+  // const redirectToManager = () => {
+  //   history.push("/manager/setMenu");
+  // };
 
-  render() {
+  
     return (
       <div className="logincontainer">
         <div>
           <h2 className="loginh2">Restaurant Login </h2>
         </div>
 
-        <div className={this.state.container} id="container">
+        <div className={container} id="container">
           <div className="form-container sign-up-container">
             <form className="loginform" action="#">
               <h1 className="colorCreateAccount loginh1">Create Account</h1>
@@ -67,7 +85,7 @@ class Login extends Component {
 
               <button
                 className="buttonclass rounded-pill m-2"
-                onClick={this.redirectToManager}
+                onClick={signUpButton}
               >
                 Sign Up
               </button>
@@ -88,18 +106,22 @@ class Login extends Component {
                 </a>
               </div> */}
               <span className="loginspan">or use your account</span>
-              <input className="logininput" type="email" placeholder="Email" />
+              <input className="logininput" type="email" placeholder="Email" onChange={(e) =>{
+                setEmail(e.target.value);
+              }}/>
               <input
                 className="logininput"
                 type="password"
                 placeholder="Password"
-              />
+                onChange={(e) =>{
+                  setPassword(e.target.value);
+                }}/>
               <a className="logina" href="#">
                 Forgot your password?
               </a>
               <button
                 className="buttonclass rounded-pill"
-                onClick={this.redirectToCustomer}
+                onClick={redirectToCustomer}
               >
                 Sign In
               </button>
@@ -115,7 +137,7 @@ class Login extends Component {
                 <button
                   className="buttonclass ghost rounded-pill"
                   id="signIn"
-                  onClick={this.signInButton}
+                  onClick={signInButton}
                 >
                   Sign In
                 </button>
@@ -128,7 +150,7 @@ class Login extends Component {
                 <button
                   className=" buttonclass ghost rounded-pill "
                   id="signUp"
-                  onClick={this.signUpButton}
+                  onClick={signInButton}
                 >
                   Sign Up
                 </button>
@@ -139,6 +161,7 @@ class Login extends Component {
       </div>
     );
   }
-}
 
-export default withRouter(Login);
+
+export default Login;
+
